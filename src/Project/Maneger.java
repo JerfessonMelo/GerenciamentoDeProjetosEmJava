03a14a90b54bf6889e220dc2584b7173a272406f
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-10-10 10:59:19 -0300

    Sistema de Gerenciamento De Projetos-Segundo Periodo ADS

---
+++ src/Project/Maneger.java
@@ -0,0 +1,30 @@
+package Project;
+
+public class Maneger extends Member {
+
+	private String department;
+	
+	public Maneger(int id, String name, String function, String email, String senha, String telephone, String projectRole) {
+		super(id, name, function, email, senha, telephone);
+        this.department = projectRole;
+	}
+	
+	public void ManagerInfo() {
+        String info = String.format("Manager Info: ID: %d, Name: %s, Function: %s, Email: %s, Telephone: %s, Department: %s",
+                getId(), getName(), getFunction(), getEmail(), getTelephone(), department);
+        System.out.println(info);
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }	
+
+    void removeMeneger(int id_remove) {
+	namesList.remove(id_remove);
+		
+	}
+}
