eafe5ad2748ba1d6530057ea9e131be3645f563a
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-10-16 19:28:30 -0300

    Adicionando uma janela para o projeto

---
+++ src/Project/Window.java
@@ -0,0 +1,11 @@
+package Project;
+import javax.swing.JFrame;
+
+public class Window {
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Project Manager");
+        window.setSize(500,500);
+        window.setVisible(true);
+    }
+}
