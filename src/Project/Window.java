0b4276e8bcdc085b137506232c947dea65fbbf95
JerfessonMelo <jerfessonmelo123@gmail.com>
2024-10-16 23:58:04 -0300

    teste

--- src/Project/Window.java
+++ src/Project/Window.java
@@ -1,11 +1,41 @@
 package Project;
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Window {
 
     public static void main(String[] args) {
-        JFrame window = new JFrame("Project Manager");
-        window.setSize(500,500);
+        JFrame window = new JFrame("Test");
+        window.setSize(640,400);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setBackground(Color.LIGHT_GRAY);
+        panel.setLayout(new FlowLayout());
+        JButton button1 = new JButton("Home");
+        JButton button2 = new JButton("Configurações");
+
+        window.add(button1);    
+        window.add(button2);
+        window.add(panel);
+        
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("Arquivos");
+        menuBar.add(menu);
+        window.setJMenuBar(menuBar);
+
+        window.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                int confirm = JOptionPane.showConfirmDialog(window, "Deseja realmente fechar?", "Confirmação", JOptionPane.YES_NO_OPTION);
+                if (confirm == JOptionPane.YES_OPTION) {
+                    window.dispose(); 
+                }
+            }
+        });
+
         window.setVisible(true);
     }
 }
